979caf7a985c0a38a39feabb4b7123f350033c43
고준영 <kojang1995@gmail.com>
2022-04-06 15:26:13 +0900

    feat(DB): DB연결 및 초기세팅

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
 import config from "./config";
+import "./database/db";
 
 app.listen(config.port, function () {
   console.log("Express listening on port", config.port);
