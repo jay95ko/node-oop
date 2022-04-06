4462e888bd207c84063179b5eda9f74da5f4a945
고준영 <kojang1995@gmail.com>
2022-04-06 09:59:49 +0900

    master(initial): 라우터 등록

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,13 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
+import Container from "typedi";
+import { StudentController } from "./controller/student";
+import { LectureController } from "./controller/lecture";
+import { EnrollmentController } from "./controller/enrollment";
+import studentRouter from "./router/student";
+import LectureRouter from "./router/lecture";
+import enrollmentRouter from "./router/enrollment";
 
 class App {
   public app: express.Application;
@@ -16,10 +23,18 @@ class App {
     this.app.use(express.json());
     this.app.use(helmet());
     this.app.use(cors());
-    this.app.use(morgan("tiny"));
+    this.app.use(morgan("dev"));
   }
 
   registerRoute() {
+    const studentController = Container.get(StudentController);
+    const lectureController = Container.get(LectureController);
+    const enrollmentController = Container.get(EnrollmentController);
+
+    this.app.use("/student", studentRouter(studentController));
+    this.app.use("/lecture", LectureRouter(lectureController));
+    this.app.use("/enrollment", enrollmentRouter(enrollmentController));
+
     this.app.use((req: Request, res: Response, next: NextFunction) => {
       res.sendStatus(404);
     });
