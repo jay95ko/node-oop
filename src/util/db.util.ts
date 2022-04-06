3e654651288421ed05d6efec3b3102817926fe53
고준영 <kojang1995@gmail.com>
2022-04-06 22:56:55 +0900

    feat(lecture): 유틸 추가

--- src/util/db.util.ts
+++ src/util/db.util.ts
@@ -1,9 +1,27 @@
-export const getColumnForQuery = (object: object) => {
+export const getAndColumnForQuery = (object: object) => {
   const entries: Array<any> = Object.entries(object);
 
   const conditions: string = entries
     .map(([key, value]) => `${key} = '${value}'`)
     .join("AND ");
+
+  return conditions;
+};
+
+export const getOrColumnForQuery = (key: string, values: Array<any>) => {
+  const conditions: string = values
+    .map((value) => `${key} = '${value}'`)
+    .join(" OR ");
+
+  return conditions;
+};
+
+export const getOrColumnForUpdateQuery = (object: object) => {
+  const entries: Array<any> = Object.entries(object);
+
+  const conditions: string = entries
+    .map(([key, value]) => `${key} = '${value}'`)
+    .join(", ");
 
   return conditions;
 };
