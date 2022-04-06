f7194498ea5f212d1a40e7fe9bb2d493439e7645
고준영 <kojang1995@gmail.com>
2022-04-06 16:01:58 +0900

    feat(student): 유틸 추가 및 변경
    1. db 쿼리 만드는 유틸 변경
    2. 현재시간 유딜 생성

--- src/util/db.util.ts
+++ src/util/db.util.ts
@@ -1,6 +1,4 @@
 export const getColumnForQuery = (object: object) => {
-  const keys: Array<string> = Object.keys(object);
-  const values: Array<string | number> = Object.values(object);
   const entries: Array<any> = Object.entries(object);
 
   const conditions: string = entries
