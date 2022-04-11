53e55e60adbf82060e6a88effe9d2f1f0468ba91
고준영 <kojang1995@gmail.com>
2022-04-11 21:55:10 +0900

    feat(unitTest): category test 추가 및 기능 수정

---
+++ src/modules/stub/repository/stub.category.repository.ts
@@ -0,0 +1,28 @@
+import { CategoryReopsitory } from "../../../database/repository/category";
+
+export class StubCategoryRepository extends CategoryReopsitory {
+  private existCategory = [
+    {
+      id: 1,
+      category: "웹",
+    },
+    {
+      id: 2,
+      category: "데이터베이스",
+    },
+  ];
+  constructor(tableName: string) {
+    super(tableName);
+  }
+
+  findListById = async (params: Array<number>) => {
+    let result: any = [];
+    this.existCategory.forEach((category) => {
+      if (params.includes(category.id) && !result.includes(category)) {
+        result.push(category);
+      }
+    });
+
+    return result;
+  };
+}
