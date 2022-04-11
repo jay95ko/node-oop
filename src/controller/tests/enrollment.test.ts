f473984e46bd7b8a37b11ffc578d5425105ece5f
고준영 <kojang1995@gmail.com>
2022-04-11 21:55:41 +0900

    feat(unitTest): enrollment test 추가 및 기능 수정

---
+++ src/controller/tests/enrollment.test.ts
@@ -0,0 +1,40 @@
+import "reflect-metadata";
+import httpMocks from "node-mocks-http";
+import Container from "typedi";
+import { EnrollmentController } from "../enrollment";
+import { StubEnrollmentService } from "../../modules/stub/service/stbu.enrollment.service";
+import { EnrollmentReopsitory } from "../../database/repository/elrollment";
+import { LectureReopsitory } from "../../database/repository/lecture";
+import { StudentReopsitory } from "../../database/repository/student";
+import Date from "../../util/date.util";
+
+describe("EnrollmentController", () => {
+  let enrollmentController: EnrollmentController;
+  beforeEach(() => {
+    const studentRepository = Container.get(StudentReopsitory);
+    const lectureRepository = Container.get(LectureReopsitory);
+    const enrollmentReopsitory = Container.get(EnrollmentReopsitory);
+    const date = Container.get(Date);
+    const stubsEnrollmentService = new StubEnrollmentService(
+      enrollmentReopsitory,
+      lectureRepository,
+      studentRepository,
+      date
+    );
+    enrollmentController = new EnrollmentController(stubsEnrollmentService);
+  });
+
+  describe("수강신청 컨트롤러", () => {
+    it('수강신청 성공 되었을 때 "Sucess create 1 of enrollment" 반환', async () => {
+      const request = httpMocks.createRequest();
+      const response = httpMocks.createResponse();
+
+      await enrollmentController.create(request, response);
+
+      expect(response.statusCode).toBe(201);
+      expect(response._getData().result).toEqual(
+        "Sucess create 1 of enrollment"
+      );
+    });
+  });
+});
--- src/modules/interface/enrollment.interface.ts
+++ src/modules/interface/enrollment.interface.ts
@@ -2,3 +2,9 @@ export interface IEnrollment {
   studentId: number;
   lectureIds: Array<number>;
 }
+
+export interface IEnrollmentInfo {
+  id: number;
+  studentId: number;
+  lectureId: number;
+}
